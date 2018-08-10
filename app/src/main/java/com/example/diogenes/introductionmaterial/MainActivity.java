2cfff609517be601a22fcd649efb33043e4aae2a
diogenesricardo <diogenesr.freitas@gmail.com>
2018-08-10 09:53:00 -0300

    Exemplo elevation

--- app/src/main/java/com/example/diogenes/introductionmaterial/MainActivity.java
+++ app/src/main/java/com/example/diogenes/introductionmaterial/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.diogenes.introductionmaterial;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.button);
+        button.setElevation(10);
+
+        SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                button.setElevation(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
